983c2d1440946ed0863994667b6091cf960be56e
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2025-10-27 17:23:26 -0300

    fix: correção de arquivos para teste.

---
+++ contracts/test/votacao.t.sol
@@ -0,0 +1,46 @@
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../src/votacao.sol";
+import "forge-std/Test.sol";
+
+contract voracaoTest is Test{
+
+    Votacao private votacao;
+    address private owner;
+    address private voter1;
+    address private voter2;
+    uint256 private totalVotos;
+    uint256 private totalCandidatos;
+    iunt256 private listaCandidatos;
+    bool private eleicaoAberta;
+    bool private jaVotou;
+
+    function setUp() public{
+
+        owner = adderss(this);
+        voter1 = address(0x123);
+        voter2 = address(0xabcd);   
+
+        votacao = new Votacao();  
+        eleicaoAberta = votacao.eleicaoAberta(true);
+    }
+
+    function testGravaVotosFalse() public{
+        vm.prank(voter1);
+
+        vm.expectRevert("Candidato nao existe.");
+        votacao.gravaVotos(10);
+    }
+
+    function testGravaVotosOk() public{
+        vm.prank(voter1);
+       
+        votacao.gravaVotos(1);
+        uint256 total = votacao.getTotalVotos(1);
+        assertEq(total, 1, "Voto nao gravado corretamente");
+    }
+
+
+
+}
